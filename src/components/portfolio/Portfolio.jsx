fbbda0a5b8de2c884405acdaefe9ca8dc8412191
Ritik Patel <102644307+ritik177@users.noreply.github.com>
2023-09-22 09:42:50 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './portfolio.css';
 import IMG1 from '../../assets/portfolio1.jpg';
+import IMG11 from '../../assets/portfolio11.jpg';
 import IMG2 from '../../assets/portfolio2.png';
 import IMG3 from '../../assets/portfolio3.jpg';
 import IMG4 from '../../assets/portfolio4.png';
