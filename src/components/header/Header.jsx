63b00371872dbf8535283c67551b68db13c45c97
Ritik Patel <ritikpatel82993@gmail.com>
2024-02-02 14:47:21 +0530

    chat app added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
+import React from "react";
 import "./header.css";
-import CTA from './CTA';
-import ME from '../../assets/developer.png';
-import HeaderSocials from './HeaderSocials';
+import CTA from "./CTA";
+import ME from "../../assets/developer.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
-      
-  <div className='container header_container'>
+      <div className="container header_container">
+        <h4>Hello I'm</h4>
+        <h1>Ritik Patel</h1>
 
-    <h4>Hello I'm</h4>
-    <h1>Ritik Patel</h1>
+        <h4 className="text-light">Fullstack Developer</h4>
+        <h4 className="text-light">(MERN Stack)</h4>
+        <CTA />
+        <HeaderSocials />
 
-   
+        <div className="me">
+          <img src={ME} alt="" />
+          {/* <img src={ME} alt="" style={{ width: "100%", maxWidth: "100px" }} /> */}
+        </div>
 
-    <h4 className="text-light">Fullstack Developer</h4>
-    <h4 className="text-light">(MERN Stack)</h4>
-  <CTA/>
-  <HeaderSocials/>
-
-     <div className="me">
-      <img src={ME} alt="" />
-     </div>
-
-     <a href="#contact" className='scroll_down'>Scroll Down</a>
-
-  </div>
-
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,113 +1,116 @@
-import React from 'react'
-import './portfolio.css';
-import IMG1 from '../../assets/portfolio1.jpg';
-import IMG11 from '../../assets/portfolio11.png';
-import IMG2 from '../../assets/portfolio2.png';
-import IMG3 from '../../assets/portfolio3.jpg';
-import IMG4 from '../../assets/portfolio4.png';
-import IMG5 from '../../assets/portfolio5.png';
-import IMG6 from '../../assets/portfolio6.png';
-import IMG7 from '../../assets/portfolio7.png';
-
-
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG11 from "../../assets/portfolio11.png";
+import IMG2 from "../../assets/portfolio2.png";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.png";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.png";
+import IMG7 from "../../assets/portfolio7.png";
+import IMG9 from "../../assets/Chat_App.png";
 
 //DO NOT USE THE IMAGES IN PRODUCTION
 
-
-const data=[
-{
-  id:1,
-  image:IMG1,
-  title:"Amazon clone",
-  github:'https://github.com/ritik177',
-  demo:'https://amazon-clone-lqqk.onrender.com/'
-},
+const data = [
   {
-  id:2,
-  image:IMG11,
-  title:"E-Commerce Website",
-  github:'https://github.com/ritik177/E-Commerce-website',
-  demo:'e-commerce-website-qzt3.vercel.app/'
-},
+    id: 1,
+    image: IMG9,
+    title: "Chat App",
+    github: "https://github.com/ritik177/ChatAp",
+    demo: "https://chatapp-g12p.onrender.com",
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: "Amazon clone",
+    github: "https://github.com/ritik177",
+    demo: "https://amazon-clone-lqqk.onrender.com/",
+  },
+  {
+    id: 3,
+    image: IMG11,
+    title: "E-Commerce Website",
+    github: "https://github.com/ritik177/E-Commerce-website",
+    demo: "e-commerce-website-qzt3.vercel.app/",
+  },
 
-{
-  id:3,
-  image:IMG2,
-  title:"CRUD Application Using MERN Stack",
-  github:'https://github.com/ritik177',
-  demo:'https://crud-frontent-ritik177.vercel.app/'
-},
+  {
+    id: 4,
+    image: IMG2,
+    title: "CRUD Application Using MERN Stack",
+    github: "https://github.com/ritik177",
+    demo: "https://crud-frontent-ritik177.vercel.app/",
+  },
 
-{
-  id:4,
-  image:IMG3,
-  title:"Book My Movie Ticket",
-  github:'https://github.com/ritik177',
-  demo:'https://book-movie-ticket-pi.vercel.app/'
-},
+  {
+    id: 5,
+    image: IMG3,
+    title: "Book My Movie Ticket",
+    github: "https://github.com/ritik177",
+    demo: "https://book-movie-ticket-pi.vercel.app/",
+  },
 
-{
-  id:5,
-  image:IMG4,
-  title:"Music App Clone",
-  github:'https://github.com/ritik177',
-  demo:'https://music-app-seven-tau.vercel.app/'
-},
+  {
+    id: 6,
+    image: IMG4,
+    title: "Music App Clone",
+    github: "https://github.com/ritik177",
+    demo: "https://music-app-seven-tau.vercel.app/",
+  },
 
-{
-  id:6,
-  image:IMG5,
-  title:"Calculator",
-  github:'https://github.com/ritik177/Calculator',
-  demo:'https://calculator-six-nu.vercel.app/'
-},
+  {
+    id: 7,
+    image: IMG5,
+    title: "Calculator",
+    github: "https://github.com/ritik177/Calculator",
+    demo: "https://calculator-six-nu.vercel.app/",
+  },
 
-{
-  id:7,
-  image:IMG6,
-  title:"Tic-Tac-Toe Game",
-  github:'https://github.com/ritik177',
-  demo:'https://tic-tac-toe-theta-dun.vercel.app/'
-},
-{
-  id:8,
-  image:IMG7,
-  title:"BirthDay_Wishes",
-  github:'https://github.com/ritik177/Birthday-Wishes',
-  demo:'https://birthday-wishes-eosin.vercel.app/'
-}
-
+  {
+    id: 8,
+    image: IMG6,
+    title: "Tic-Tac-Toe Game",
+    github: "https://github.com/ritik177",
+    demo: "https://tic-tac-toe-theta-dun.vercel.app/",
+  },
+  {
+    id: 9,
+    image: IMG7,
+    title: "BirthDay_Wishes",
+    github: "https://github.com/ritik177/Birthday-Wishes",
+    demo: "https://birthday-wishes-eosin.vercel.app/",
+  },
 ];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Projects</h2>
 
       <div className="container portfolio_container">
-        
-        {
-          data.map(({id,image,title,github,demo})=>{
-            return(
-              <article key={id} className="portfolio_item">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
               <div className="portfolio_item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
-             <div className="portfolio_item-cta">
-             <a href={github} className='btn' target='_blank'>GitHub</a>
-              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
-             </div>
+              <div className="portfolio_item-cta">
+                <a href={github} className="btn" target="_blank">
+                  GitHub
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
             </article>
-            )
-          })
-        }
-  
+          );
+        })}
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
