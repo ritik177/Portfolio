e97eef7ae990dedcbb43727c3b589a5fba211796
Ritik Patel <ritikpatel82993@gmail.com>
2024-02-02 14:59:00 +0530

    resume update added

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import CV from '../../assets/cv.pdf';
-
+import React from "react";
+import CV from "../../assets/cv.pdf";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        {/* <a href={CV} download className='btn'>Download CV</a> */}
-          <a href="https://drive.google.com/file/d/1ZYQNAGu4ubuNMZV7rpkxEXIokpFuOFSX/view?usp=sharing" target="_blank" className="btn"> Download CV </a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    <div className="cta">
+      {/* <a href={CV} download className='btn'>Download CV</a> */}
+      <a
+        href="https://drive.google.com/file/d/1v7PsiGhBCtqpc6IcoWXq_Bo2ofod6LeP/view?usp=sharing"
+        target="_blank"
+        className="btn"
+      >
+        {" "}
+        Download CV{" "}
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
